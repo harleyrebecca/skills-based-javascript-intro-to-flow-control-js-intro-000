808f76a9f64d2aed81c2301529f64e771ad60c71
Harley Watkins <harleywatkins99@gmail.com>
2021-04-12 02:01:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,6 @@
-function basicTeenager(age) {
-  describe('basicTeenager', () => {
-    it('should return "You are a teenager!" if the age is between 13-19', () => {
-      expect(basicTeenager(13)).toEqual("You are a teenager!");
-    })
+function basicTeenager(age) () => {
+if (age>=13 && age<=19)}
 
-    it('should return undefined if the age is not between 13-19', () => {
-      expect(basicTeenager(12)).toBe(undefined)
-    })
-
-  })
 
 function teenager(age) {
 
